9fca74afd2b9a0493b5146d7d345050875ed3097
Shreyask96 <shreyaskashappgoudar@gmail.com>
2024-09-28 22:20:57 +0530

    Created a Junit Project

---
+++ src/main/java/com/Junit/CURDOperationsTest/CurdOperationsTestApplication.java
@@ -0,0 +1,13 @@
+package com.Junit.CURDOperationsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdOperationsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdOperationsTestApplication.class, args);
+	}
+
+}
